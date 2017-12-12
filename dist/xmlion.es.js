ff6f768da5a5a0805e43a17dd1396a831fb020ea
Bryan Maamo <ninjabryan@gmail.com>
2017-12-12 08:31:10 +0000

    microbundle build

--- dist/xmlion.es.js
+++ dist/xmlion.es.js
@@ -1,152 +1,136 @@
-const {isArray} = Array;
-const clone = (array) => array.slice(0);
-const join = (array) => array.join('');
-const map = (fn, array) => array.map(fn);
-const filter = (fn, array) => array.filter(fn);
-const concat = (array, ...arrays) => array.concat(...arrays);
-const toStringCub = (c) => c == null ? '' : c.ROARS ? c._buildElementString() : c;
-const mapCubsToString = (cubs) => map(toStringCub, cubs);
+var isArray = Array.isArray;
+var clone = function (array) { return array.slice(0); };
+var join = function (array) { return array.join(''); };
+var map = function (fn, array) { return array.map(fn); };
+var filter = function (fn, array) { return array.filter(fn); };
+var concat = function (array) {
+    var arrays = [], len = arguments.length - 1;
+    while ( len-- > 0 ) arrays[ len ] = arguments[ len + 1 ];
 
-class Lion {
-  constructor (tagName, attributes, cubs) {
+    return array.concat.apply(array, arrays);
+};
+var toStringCub = function (c) { return c == null ? '' : c.ROARS ? c._buildElementString() : c; };
+var mapCubsToString = function (cubs) { return map(toStringCub, cubs); };
+var Lion = function Lion(tagName, attributes, cubs) {
     this.tagName = tagName;
     this.attr = attributes || {};
     this.cubs = cubs || [];
-  }
+};
 
-  get ROARS () {
+var prototypeAccessors = { ROARS: { configurable: true },cubs: { configurable: true },attributes: { configurable: true } };
+prototypeAccessors.ROARS.get = function () {
     return true;
-  }
-
-  set cubs (cubs) {
+};
+prototypeAccessors.cubs.set = function (cubs) {
     if (isArray(cubs)) {
-      this._cubs = cubs;
+        this._cubs = cubs;
     } else {
-      throw new TypeError('Lion wants cub[]');
+        throw new TypeError('Lion wants cub[]');
     }
-  }
-
-  get cubs () {
+};
+prototypeAccessors.cubs.get = function () {
     return clone(this._cubs);
-  }
-
-  set attributes (attr) {
+};
+prototypeAccessors.attributes.set = function (attr) {
     this.attr = attr;
-  }
-
-  get attributes () {
+};
+prototypeAccessors.attributes.get = function () {
     return this.attr;
-  }
-
-  setCubs (cubs) {
+};
+Lion.prototype.setCubs = function setCubs (cubs) {
     this.cubs = cubs;
     return this;
-  }
-
-  getCubs () {
+};
+Lion.prototype.getCubs = function getCubs () {
     return this.cubs;
-  }
-
-  setAttributes (attr) {
+};
+Lion.prototype.setAttributes = function setAttributes (attr) {
     this.attributes = attr;
     return this;
-  }
-
-  getAttributes () {
+};
+Lion.prototype.getAttributes = function getAttributes () {
     return this.attributes;
-  }
-
-  handleBuildAttribute (key, value) {
+};
+Lion.prototype.handleBuildAttribute = function handleBuildAttribute (key, value) {
     if (value === null || value === undefined) {
-      return key;
+        return key;
     }
-    return `${key}="${value}"`;
-  }
+    return (key + "=\"" + value + "\"");
+};
+Lion.prototype._buildAttributeString = function _buildAttributeString () {
+        var this$1 = this;
 
-  _buildAttributeString () {
-    const attr = this.attr;
-    const keys = Object.keys(attr).sort();
+    var attr = this.attr;
+    var keys = Object.keys(attr).sort();
     if (keys.length > 0) {
-      return join(map((key) => ` ${this.handleBuildAttribute(key, attr[key])}`, keys));
+        return join(map(function (key) { return (" " + (this$1.handleBuildAttribute(key, attr[key]))); }, keys));
     }
     return '';
-  }
-
-  _buildContentString () {
+};
+Lion.prototype._buildContentString = function _buildContentString () {
     return join(mapCubsToString(this._cubs));
-  }
-
-  _buildElementString () {
-    const me = this;
-    const tagName = me.tagName;
-    const attributes = me._buildAttributeString();
-
+};
+Lion.prototype._buildElementString = function _buildElementString () {
+    var me = this;
+    var tagName = me.tagName;
+    var attributes = me._buildAttributeString();
     if (me.hasCubs()) {
-      const content = me._buildContentString();
-
-      return `<${tagName}${attributes}>${content}</${tagName}>`;
+        var content = me._buildContentString();
+        return ("<" + tagName + attributes + ">" + content + "</" + tagName + ">");
     } else {
-      return `<${tagName}${attributes}/>`;
+        return ("<" + tagName + attributes + "/>");
     }
-  }
-
-  addAttribute (name, value) {
-    const lion = this;
+};
+Lion.prototype.addAttribute = function addAttribute (name, value) {
+    var lion = this;
     lion.attr[name] = value;
     return lion;
-  }
-
-  removeAttribute (name) {
-    const lion = this;
+};
+Lion.prototype.removeAttribute = function removeAttribute (name) {
+    var lion = this;
     if (lion.attr[name]) {
-      delete lion.attr[name];
+        delete lion.attr[name];
     }
     return lion;
-  }
-
-  addAttributes (attr) {
-    const lion = this;
-    const names = Object.keys(attr);
-    map((name) => lion.addAttribute(name, attr[name]), names);
+};
+Lion.prototype.addAttributes = function addAttributes (attr) {
+    var lion = this;
+    var names = Object.keys(attr);
+    map(function (name) { return lion.addAttribute(name, attr[name]); }, names);
     return lion;
-  }
-
-  addCub (cub) {
-    const lion = this;
+};
+Lion.prototype.addCub = function addCub (cub) {
+    var lion = this;
     lion._cubs.push(cub);
     return lion;
-  }
-
-  removeCub (cub) {
-    let lion = this;
-    let index = lion._cubs.indexOf(cub);
+};
+Lion.prototype.removeCub = function removeCub (cub) {
+    var lion = this;
+    var index = lion._cubs.indexOf(cub);
     if (index >= 0) {
-      lion.cubs = filter((c, i) => (i !== index), lion.cubs);
+        lion.cubs = filter(function (c, i) { return i !== index; }, lion.cubs);
     }
     return lion;
-  }
-
-  addCubs (cubs) {
+};
+Lion.prototype.addCubs = function addCubs (cubs) {
     if (isArray(cubs)) {
-      this.cubs = concat(this.cubs, cubs);
+        this.cubs = concat(this.cubs, cubs);
     }
     return this;
-  }
-
-  removeAllCubs () {
+};
+Lion.prototype.removeAllCubs = function removeAllCubs () {
     this._cubs = [];
     return this;
-  }
-
-  hasCubs () {
+};
+Lion.prototype.hasCubs = function hasCubs () {
     return this._cubs && this._cubs.length > 0;
-  }
-
-  value () {
+};
+Lion.prototype.value = function value () {
     return this._buildElementString();
-  }
-}
+};
 
-const xmlion = (tagName, attributes, cubs) => new Lion(tagName, attributes, cubs);
+Object.defineProperties( Lion.prototype, prototypeAccessors );
+export default function (tagName, attributes, cubs) { return new Lion(tagName, attributes, cubs); };
 
 export { Lion, xmlion };
+//# sourceMappingURL=xmlion.es.js.map
